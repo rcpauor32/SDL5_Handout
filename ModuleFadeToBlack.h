ea1565365081a1c47d66fd5af17bbb6bfd46decc
rcpauor32 <rcolivepau@gmail.com>
2016-04-04 16:58:09 +0200

    Created SDL5 Handout

---
+++ ModuleFadeToBlack.h
@@ -0,0 +1,29 @@
+#ifndef __MODULEFADETOBLACK_H__
+#define __MODULEFADETOBLACK_H__
+
+#include "Module.h"
+#include "SDL\include\SDL_rect.h"
+
+class ModuleFadeToBlack : public Module
+{
+public:
+	ModuleFadeToBlack();
+	~ModuleFadeToBlack();
+
+	bool Start();
+	update_status Update();
+	void FadeToBlack(Module* module_off, Module* module_on, float time = 2.0f);
+
+private:
+
+	Uint32 start_time = 0;
+	Uint32 total_time = 0;
+	SDL_Rect screen;
+	bool fading_in = true;
+	bool already_fading = false;
+	Module* module_in;
+	Module* module_out;
+
+};
+
+#endif //__MODULEFADETOBLACK_H__
